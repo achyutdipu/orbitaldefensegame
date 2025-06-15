caa15cc565e9d9f1105c450d3f0afdb9283141f8
achyutdipu <achyutdipu@gmail.com>
2025-06-15 11:52:14 -0400

    added the basic multiplayer and singleplayer choices

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div id="Background" style={{background: "black", height: "100vh", width: "100vw"}}>
-      
+    <div id="Background" style={{background: "black", height: "10vh", width: "10vw", padding: "w"}}>
+      <div id='startbox' className='startbox'>
+        <button id='singleplayer' className='startbutton'>Single Player</button>
+        <br />
+        <br />
+        <button id='multiplayer' className='startbutton'>Multiplayer</button> 
+        /*Multiplayer is not supported yet */
+      </div>
     </div>
   );
 }
