24fa63f25e1904372bc15955e61983ee8667c730
achyutdipu <achyutdipu@gmail.com>
2025-06-16 09:52:45 -0400

    adding the logo and player 1 avatar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import {ReactComponent as Player1} from './assets/clean/player1-clean.svg';
 import './App.css';
 
 function App() {
   return (
-    <div id="Background" style={{background: "black", height: "10vh", width: "10vw", padding: "w"}}>
+    <div id="Background" style={{background: "black", height: "10vh", width: "10vw", padding: "45vh 45vw"}}>
+      {/* <Player1 style={{width: "100px", height: "100px"}} /> */}
       <div id='startbox' className='startbox'>
         <button id='singleplayer' className='startbutton'>Single Player</button>
         <br />
