e4eb82a9b78db123583c72f88b7d2ddfd8136424
achyutdipu <achyutdipu@gmail.com>
2025-06-21 10:18:55 -0400

    censoring api key

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAutefzlExMAOvbifI3Xb1xuWQVOO2PUQ0",
+  apiKey: "***************************************",
   authDomain: "orbitaldefensegame-6e898.firebaseapp.com",
   projectId: "orbitaldefensegame-6e898",
   storageBucket: "orbitaldefensegame-6e898.firebasestorage.app",
